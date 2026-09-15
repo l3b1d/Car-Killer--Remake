7dc43361de0d51861c5d939c21922b3c8dd57f41
l3b1d <81329053+l3b1d@users.noreply.github.com>
2026-09-16 01:21:15 +0300

    Restructure UI scenes and add settings buttons
    Did some refactoring

--- scripts/UI/main_menu/main_menu.gd
+++ scripts/UI/main_menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 ## For Quit Button
-@onready var quit_btn: Button = $mainMenuBckgr/VBoxContainer/quitBtn
+@onready var quit_btn: Button = $mainMenuBckgr/VBoxContainer/quit_btn
 
 func _quitBtnReady() -> void:
 	quit_btn.pressed.connect(_on_quit_btn_pressed)
@@ -12,24 +12,24 @@ func _on_quit_btn_pressed() -> void:
 ## For Quit Button
 
 ## For Start Button
-@onready var start_btn: Button = $mainMenuBckgr/VBoxContainer/startBtn
+@onready var start_btn: Button = $mainMenuBckgr/VBoxContainer/start_btn
 
 func _startBtnReady() -> void:
 	start_btn.pressed.connect(_on_start_btn_pressed)
 
 func _on_start_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/3D/baseGame.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/base_game.tscn")
 
 ## For Start Button
 
 ## For Settings Button
 
-@onready var settings_btn: Button = $mainMenuBckgr/VBoxContainer/settingsBtn
+@onready var settings_btn: Button = $mainMenuBckgr/VBoxContainer/settings_btn
 
 func _settingsBtnReady() -> void:
 	settings_btn.pressed.connect(_on_settings_btn_pressed)
 
 func _on_settings_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/2D/settings.tscn")
+	get_tree().change_scene_to_file("res://scenes/UI/settings.tscn")
 
 ## For Settings Button
---
+++ scripts/UI/settings/settings.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+#Save and Exit Button. W.I.P
+
+@onready var san_btn: Button = $settingsBckgr/san_btn
+
+func _san_btn_ready() -> void:
+	san_btn.pressed.connect(_on_san_btn_pressed)
+
+func _on_san_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
