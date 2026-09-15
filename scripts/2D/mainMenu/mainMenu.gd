3d15ddfdd5a1127c09d31f3e74696a8b6d53a4b2
l3b1d <81329053+l3b1d@users.noreply.github.com>
2026-09-16 00:42:43 +0300

    Made working Start button

--- scripts/2D/mainMenu/mainMenu.gd
+++ scripts/2D/mainMenu/mainMenu.gd
@@ -3,10 +3,19 @@ extends Node2D
 ## For Quit Button
 @onready var quit_btn: Button = $mainMenuBckgr/VBoxContainer/quitBtn
 
-func _ready() -> void:
+func _quitBtnReady() -> void:
 	quit_btn.pressed.connect(_on_quit_btn_pressed)
-	
+
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
 	
 ## For Quit Button
+
+## For Start Button
+@onready var start_btn: Button = $mainMenuBckgr/VBoxContainer/startBtn
+
+func _startBtnReady() -> void:
+	start_btn.pressed.connect(_on_start_btn_pressed)
+
+func _on_start_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/3D/baseGame.tscn")
