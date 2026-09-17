f1c5b73bc91c7c1c8eea3cb702eeab6de44921ae
l3b1d <81329053+l3b1d@users.noreply.github.com>
2026-09-17 08:38:55 +0300

    Updated car visuals, hitbox, and small tweaks into movement

--- scripts/enemies/killer_car_movement.gd
+++ scripts/enemies/killer_car_movement.gd
@@ -1,37 +1,38 @@
 extends Node
 
 @export var mass := 1400.0
-@export var max_speed := 12.0
+@export var max_speed := 16.0
 @export var engine_force := 14000.0
 @export var brake_force := 22000.0
 @export var stopping_distance := 2.5
 @export var gravity := 9.8
 @export var wheel_base := 2.5
-@export var wheel_radius := 0.39
+@export var wheel_radius := 0.3625
 @export var max_steering_angle := 0.45
 @export var steering_response := 1.6
 @export var max_turn_rate := 1.15
 @export var turn_response := 2.0
 @export var lateral_grip := 3.5
-@export var reverse_speed := 3.0
+@export var reverse_speed := 3.6
 @export var reverse_angle_threshold := 1.65
 
 @onready var car: CharacterBody3D = get_parent()
 @onready var hit_reaction: Node = car.get_node("hit_reaction")
 @onready var model: Node3D = car.get_node("killer_car_model")
-@onready var front_left_steer: Node3D = model.get_node("front_left_wheel_steer")
-@onready var front_right_steer: Node3D = model.get_node("front_right_wheel_steer")
-@onready var front_left_wheel: Node3D = model.get_node("front_left_wheel_steer/wheel_front_left")
-@onready var front_right_wheel: Node3D = model.get_node("front_right_wheel_steer/wheel_front_right")
-@onready var rear_left_wheel: Node3D = model.get_node("wheel_rear_left")
-@onready var rear_right_wheel: Node3D = model.get_node("wheel_rear_right")
+@onready var front_left_wheel: Node3D = model.find_child("LFrontWheel", true, false) as Node3D
+@onready var front_right_wheel: Node3D = model.find_child("RFrontWheel", true, false) as Node3D
+@onready var rear_left_wheel: Node3D = model.find_child("LBackWheel", true, false) as Node3D
+@onready var rear_right_wheel: Node3D = model.find_child("RBackWheel", true, false) as Node3D
 
 var target: Node3D
 var drive_velocity := Vector3.ZERO
 var steering_angle := 0.0
 var turn_rate := 0.0
+var rest_bases: Dictionary[Node3D, Basis] = {}
 
 func _ready() -> void:
+	for wheel in [front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel]:
+		rest_bases[wheel] = wheel.basis
 	target = get_tree().get_first_node_in_group("player") as Node3D
 	call_deferred("_align_to_target")
 
@@ -110,11 +111,11 @@ func _apply_gravity(delta: float) -> void:
 		car.velocity.y = 0.0
 
 func _update_wheels(delta: float, forward_speed: float) -> void:
-	front_left_steer.rotation.y = steering_angle
-	front_right_steer.rotation.y = steering_angle
-
 	var wheel_roll := forward_speed * delta / wheel_radius
-	front_left_wheel.rotate_object_local(Vector3.UP, -wheel_roll)
-	front_right_wheel.rotate_object_local(Vector3.UP, -wheel_roll)
-	rear_left_wheel.rotate_object_local(Vector3.UP, -wheel_roll)
-	rear_right_wheel.rotate_object_local(Vector3.UP, -wheel_roll)
+	_pose_wheel(front_left_wheel, steering_angle, wheel_roll)
+	_pose_wheel(front_right_wheel, steering_angle, wheel_roll)
+	_pose_wheel(rear_left_wheel, 0.0, wheel_roll)
+	_pose_wheel(rear_right_wheel, 0.0, wheel_roll)
+
+func _pose_wheel(wheel: Node3D, steer: float, spin: float) -> void:
+	wheel.basis = Basis(Vector3.UP, steer) * rest_bases[wheel] * Basis(Vector3.UP, spin)
